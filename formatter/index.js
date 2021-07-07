3f7ef8fb35b71ecfb5ea85398cca13320c554839
Dennis Schoepf <me@dnsc.io>
2021-07-07 10:45:59 +0200

    Update prettier config

--- formatter/index.js
+++ formatter/index.js
@@ -1,8 +1,7 @@
 module.exports = {
-  trailingComma: 'es5',
+  trailingComma: "es5",
   tabWidth: 2,
   singleQuote: true,
   jsxBracketSameLine: true,
   semi: true,
-  importOrderSeparation: false,
-}
+};
