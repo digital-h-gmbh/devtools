b73ee4da5f9da32a0a73609b38a05af67f290864
Dennis Schoepf <d.schoepf@digital-h.de>
2021-10-22 11:49:14 +0200

    Adds react native rules and bumps version

--- linter/react-native-config.js
+++ linter/react-native-config.js
@@ -11,5 +11,9 @@ module.exports = {
     "react/prefer-stateless-function": 2,
     "react/jsx-max-depth": [2, { max: 4 }],
     "max-lines": [1, { max: 175, skipBlankLines: true, skipComments: true }],
+    "react-native/no-unused-styles": 2,
+    "react-native/no-inline-styles": 2,
+    "react-native/no-color-literals": 2,
+    "react-native/no-single-element-style-arrays": 2,
   },
 };
