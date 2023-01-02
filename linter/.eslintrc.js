43acf6db52c3465d6d915ad57da82dd47572f954
Dennis Schoepf <me@dnsc.io>
2023-01-02 15:59:42 +0100

    Sets up eslint for eslint plugin

---
+++ linter/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:eslint-plugin/recommended",
+    "plugin:node/recommended"
+  ],
+  "overrides": [
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "script"
+  },
+  "rules": {
+  }
+}
