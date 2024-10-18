d556340e34a99350289720e45c97556c0eabb3b8
Hanne Hofmann <h.hofmann@digital-h.de>
2024-10-18 07:53:46 +0200

    chore(RIDE-1776): update prettier
    extend prettier configuration and update to latest version

--- formatter/index.js
+++ formatter/index.js
@@ -3,4 +3,22 @@ module.exports = {
   tabWidth: 2,
   singleQuote: true,
   semi: true,
+  printWidth: 120,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  endOfLine: "lf",
+  overrides: [
+    {
+      files: "*.yml",
+      options: {
+        tabWidth: 2
+      }
+    }
+  ],
+  plugins: ["prettier-plugin-tailwindcss", "@trivago/prettier-plugin-sort-imports", "prettier-plugin-packagejson"],
+  importOrderParserPlugins: ["typescript", "decorators-legacy", "jsx"],
+  importOrder: ["^@digital-h/(.*)$", "^@/Shared/(.*)$", "^@/(.*)$", "^[./]"],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true
 };
