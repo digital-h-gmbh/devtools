9897637fc49f423e09f3237d7192e41c7ebbf18d
Hanne Hofmann <h.hofmann@digital-h.de>
2024-10-29 07:25:49 +0100

    chore(RIDE-1840): add prettier and run format

--- linter/eslint.config.js
+++ linter/eslint.config.js
@@ -1,85 +1,97 @@
-import globals from "globals";
-
-import eslint from "eslint";
-import js from "@eslint/js";
-import esImport from "eslint-plugin-import";
-import unusedImports from "eslint-plugin-unused-imports";
-
+import js from '@eslint/js';
 import ts from '@typescript-eslint/eslint-plugin';
-import tsParser from "@typescript-eslint/parser";
+import tsParser from '@typescript-eslint/parser';
+import eslint from 'eslint';
+import esImport from 'eslint-plugin-import';
+import unusedImports from 'eslint-plugin-unused-imports';
+import globals from 'globals';
 
 export default [
-    eslint.configs.recommended,
-    js.configs.recommended,
-{
-    ignores: ["**/eslint.config.js"],
-}, {
+  eslint.configs.recommended,
+  js.configs.recommended,
+  {
+    ignores: ['**/eslint.config.js'],
+  },
+  {
     plugins: {
-        "@typescript-eslint": ts,
-        "unused-imports": unusedImports,
-        'import': esImport,
+      '@typescript-eslint': ts,
+      'unused-imports': unusedImports,
+      import: esImport,
     },
 
     languageOptions: {
-        parser: tsParser,
+      parser: tsParser,
 
-        parserOptions: {
-            project: "tsconfig.json",
-            // tsconfigRootDir: __dirname,
-            ecmaVersion: 'latest',
-        },
+      parserOptions: {
+        project: 'tsconfig.json',
+        // tsconfigRootDir: __dirname,
+        ecmaVersion: 'latest',
+      },
 
-        globals: {
-            ...globals.node,
-            ...globals.browser,
-            ...globals.jest,
-        },
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.jest,
+      },
     },
 
     rules: {
-        ...eslint.configs.recommended.rules,
-        ...ts.configs.recommended.rules,
-        "no-undef": "off",
-        "max-lines-per-function": ["error", {
-            max: 80,
-            skipComments: true,
-        }],
-        "max-depth": ["error", 3],
-        "max-nested-callbacks": ["error", 3],
-        "id-length": ["error", {
-            min: 2,
-            exceptions: ["x", "y", "t", "f", "m", "n", "d", "i", "_"],
-        }],
-        "no-shadow": "off",
-        "no-catch-shadow": "off",
-        curly: ["error", "all"],
-        "@typescript-eslint/ban-ts-comment": "warn",
-        "@typescript-eslint/explicit-module-boundary-types": "error",
-        "@typescript-eslint/no-explicit-any": "off",
-        eqeqeq: ["error", "always"],
-        "import/no-default-export": "error",
-        "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": "warn",
-        "@typescript-eslint/no-unused-vars": ["warn", {
-            vars: "all",
-            varsIgnorePattern: "^_",
-            args: "after-used",
-            argsIgnorePattern: "^_",
-        }],
-        "unused-imports/no-unused-imports": "error",
-        "unused-imports/no-unused-vars": ["warn", {
-            vars: "all",
-            varsIgnorePattern: "^_",
-            args: "after-used",
-            argsIgnorePattern: "^_",
-        }],
+      ...eslint.configs.recommended.rules,
+      ...ts.configs.recommended.rules,
+      'no-undef': 'off',
+      'max-lines-per-function': [
+        'error',
+        {
+          max: 80,
+          skipComments: true,
+        },
+      ],
+      'max-depth': ['error', 3],
+      'max-nested-callbacks': ['error', 3],
+      'id-length': [
+        'error',
+        {
+          min: 2,
+          exceptions: ['x', 'y', 't', 'f', 'm', 'n', 'd', 'i', '_'],
+        },
+      ],
+      'no-shadow': 'off',
+      'no-catch-shadow': 'off',
+      curly: ['error', 'all'],
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      eqeqeq: ['error', 'always'],
+      'import/no-default-export': 'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
     },
-}, 
-{
-    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     rules: {
-        "no-extra-semi": "off",
-        "no-mixed-spaces-and-tabs": "off",
-        "no-unexpected-multiline": "off",
+      'no-extra-semi': 'off',
+      'no-mixed-spaces-and-tabs': 'off',
+      'no-unexpected-multiline': 'off',
     },
-}];
+  },
+];
